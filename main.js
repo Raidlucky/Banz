8502922dcfd0281275504c6d646b6a915b9763b7
Lucky <swordslayer07@gmail.com>
2023-11-20 15:22:58 -0700

    Responsive Tablet Design
    Just made it so it fits on slightly smaller screens. Like tablets. Next is phones.

--- main.js
+++ main.js
@@ -1,25 +1,39 @@
-anime({
-    targets: '#dream',
-    translateX: [-270, 0],
-    opacity: [0, 1],
-    delay: anime.stagger(100)
-});
+let screenWidth = window.innerWidth;
 
-anime({
-    targets: 'nav',
-    translateY: [-100, 0],
-    opacity: [0, 1],
-    easing: 'spring(1, 70, 10, 0)',
-    duration: 200
-});
+if (screenWidth <= 993) {
+    console.log(screenWidth);
+    anime({
+        targets: '#dream',
+        translateY: [-270, 0],
+        opacity: [0, 1],
+        delay: anime.stagger(100)
+    });
+} else {
+    anime({
+        targets: '#dream',
+        translateX: [-270, 0],
+        opacity: [0, 1],
+        delay: anime.stagger(100)
+    });
+    
+    anime({
+        targets: 'nav',
+        translateY: [-100, 0],
+        opacity: [0, 1],
+        easing: 'spring(1, 70, 10, 0)',
+        duration: 200
+    });
+    
+    anime({
+        targets: '.card',
+        translateY: [270, 0],
+        opacity: [0, 1],
+        easing: 'spring(1, 50, 10, 0)'
+    });
+}
 
-anime({
-    targets: '.card',
-    translateY: [270, 0],
-    opacity: [0, 1],
-    easing: 'spring(1, 50, 10, 0)'
-});
 
 function wait() {
-    alert("This page is still under construction. Please come back later.")
+    alert("This page is still under construction. Please come back later.");
 }
+
